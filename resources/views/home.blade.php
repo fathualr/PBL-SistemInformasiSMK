10952769a428fefa99f0707e538d48f15e5ababf
Vexonor <vexonordark@gmail.com>
2024-03-07 10:44:11 +0700

    Update Layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+@section('main')
+
+<div class="grid grid-rows-3 grid-flow-col gap-4">
+    <div class="row-span-3">
+        <iframe class="w-full aspect-video" src="https://www.youtube.com/watch?v=r8J3EZC3Jqg"></iframe>
+
+    </div>
+    <div class="col-span-2 ...">
+        <video src="https://www.youtube.com/watch?v=r8J3EZC3Jqg"></video>
+    </div>
+    <div class="row-span-2 col-span-2 ...">03</div>
+</div>
+
+
+@endsection
