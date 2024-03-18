6eff81a6594335d896773fa7217d699b9c9d74ca
SalmaA096 <salmaaulia270604@gmail.com>
2024-03-18 23:46:01 +0700

    mengerjakan medsos dan prestasi siswa

--- resources/views/media-sosial.blade.php
+++ resources/views/media-sosial.blade.php
@@ -1,3 +1,139 @@
-<div>
-    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
+@extends('layouts.main')
+@section('Main')
+
+<style>
+    .social-media-icon {
+        width: 40px;
+        height: 40px;
+        margin-right: 10px;
+    }
+</style>
+
+<div class="container mx-auto p-8">
+
+    <h2 class="text-xl font-bold mb-8 text-center divider">Lokasi Sekolah</h2>
+    <div class="artboard artboard-horizontal phone-5 bg-slate-600 flex justify-center mx-auto">667×375</div>
+
+    <h2 class="text-xl font-bold mb-8 text-center my-5 divider">Temui Kami</h2>
+    <div class="grid grid-cols-2 grid-flow-col grid-rows-4 gap-5 my-10">
+
+        <div class="card card-side bg-base-100 shadow-xl h-32 ">
+            <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" class="w-32" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">New movie is released!</h2>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary h-5">Watch</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card card-side bg-base-100 shadow-xl h-32 ">
+            <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" class="w-32" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">New movie is released!</h2>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary h-5">Watch</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card card-side bg-base-100 shadow-xl h-32 ">
+            <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" class="w-32" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">New movie is released!</h2>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary h-5">Watch</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card card-side bg-base-100 shadow-xl h-32 ">
+            <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" class="w-32" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">New movie is released!</h2>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary h-5">Watch</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card card-side bg-base-100 shadow-xl h-32 ">
+            <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" class="w-32" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">New movie is released!</h2>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary h-5">Watch</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card card-side bg-base-100 shadow-xl h-32 ">
+            <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" class="w-32" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">New movie is released!</h2>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary h-5">Watch</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card card-side bg-base-100 shadow-xl h-32 ">
+            <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" class="w-32" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">New movie is released!</h2>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary h-5">Watch</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <h2 class="text-2xl font-bold mb-8 text-center divider">Hubungi Kami</h2>
+    <div class="grid grid-cols-2 gap-5">
+        <div class="col-span-1">
+            <label class="form-control w-full">
+                <div class="label">
+                    <span class="label-text">Nama</span>
+                </div>
+                <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+            </label>
+        </div>
+
+        <div class="col-span-1">
+            <label class="form-control w-full">
+                <div class="label">
+                    <span class="label-text">Email</span>
+                </div>
+                <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+            </label>
+        </div>
+
+        <div class="col-span-2">
+            <label class="form-control w-full">
+                <div class="label">
+                    <span class="label-text">No Telp</span>
+                </div>
+                <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+            </label>
+        </div>
+
+        <div class="col-span-2">
+            <label class="form-control w-full">
+                <div class="label">
+                    <span class="label-text">Isi Pesan</span>
+                </div>
+                <textarea class="textarea textarea-bordered h-48" placeholder="Bio"></textarea>
+            </label>
+        </div>
+
+    </div>
+
+    <div class="col-span-2 flex justify-center">
+        <button class="btn bg-elm mx-auto md:mx-0 md:w-48 h-10 rounded-sm border-none text-white mt-8 hover:text-elm">Kirim</button>
+    </div>
+
 </div>
+
+@endsection
--- resources/views/prestasi.blade.php
+++ resources/views/prestasi.blade.php
@@ -1,3 +1,58 @@
-<div>
-    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
+@extends('layouts.main')
+@section('Main')
+
+<?php
+
+$current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+$total_pages = 3;
+
+$previous_page = ($current_page > 1) ? ($current_page - 1) : 1;
+$next_page = ($current_page < $total_pages) ? ($current_page + 1) : $total_pages;
+?>
+
+<div class="grid grid-cols-1 gap-y-10">
+
+<div class="card lg:card-side bg-base-100 shadow-xl">
+  <img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" />
+  <div class="card-body">
+    <h2 class="card-title text-2xl font-bold my-5">JUARA 1 LOMBA CERDAS CERMAT</h2>
+    <p>Tim TRPL SMK Negeri meraih juara 1 dalam Lomba Cerdas Cermat yang diadakan pada tanggal XX/XX/XXXX. Prestasi ini menunjukkan keunggulan siswa-siswi dalam bidang pengetahuan umum dan kemampuan berpikir cepat.</p>
+    <div class="card-actions justify-end">
+      <button class="btn bg-elm mx-auto md:mx-0 md:w-48 h-10 rounded-sm border-none text-white mt-8 hover:text-elm">READ MORE >></button>
+    </div>
+  </div>
 </div>
+
+<div class="card lg:card-side bg-base-100 shadow-xl">
+  <img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" />
+  <div class="card-body">
+    <h2 class="card-title text-2xl font-bold my-5">JUARA 1 LOMBA CERDAS CERMAT</h2>
+    <p>Tim TRPL SMK Negeri meraih juara 1 dalam Lomba Cerdas Cermat yang diadakan pada tanggal XX/XX/XXXX. Prestasi ini menunjukkan keunggulan siswa-siswi dalam bidang pengetahuan umum dan kemampuan berpikir cepat.</p>
+    <div class="card-actions justify-end">
+      <button class="btn bg-elm mx-auto md:mx-0 md:w-48 h-10 rounded-sm border-none text-white mt-8 hover:text-elm">READ MORE >></button>
+    </div>
+  </div>
+</div>
+
+<div class="card lg:card-side bg-base-100 shadow-xl">
+  <img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" />
+  <div class="card-body">
+    <h2 class="card-title text-2xl font-bold my-5">JUARA 1 LOMBA CERDAS CERMAT</h2>
+    <p>Tim TRPL SMK Negeri meraih juara 1 dalam Lomba Cerdas Cermat yang diadakan pada tanggal XX/XX/XXXX. Prestasi ini menunjukkan keunggulan siswa-siswi dalam bidang pengetahuan umum dan kemampuan berpikir cepat.</p>
+    <div class="card-actions justify-end">
+      <button class="btn bg-elm mx-auto md:mx-0 md:w-48 h-10 rounded-sm border-none text-white mt-8 hover:text-elm">READ MORE >></button>
+    </div>
+  </div>
+</div>
+
+</div>
+
+<div class="join flex justify-center my-16">
+  <a href="?page=<?php echo $previous_page; ?>" class="join-item btn" <?php if ($current_page == 1) echo "disabled"; ?>>«</a>
+  <button class="join-item btn"><?php echo "Page $current_page"; ?></button>
+  <a href="?page=<?php echo $next_page; ?>" class="join-item btn" <?php if ($current_page == $total_pages) echo "disabled"; ?>>»</a>
+</div>
+
+
+@endsection
