8e812136cce4f334c372b9d38cd3cbbff799596c
Vexonor <vexonordark@gmail.com>
2024-03-24 12:29:11 +0700

    Make View For Dashboard

---
+++ app/Http/Controllers/adminController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class adminController extends Controller
+{
+    public function dashboard(){
+        return view('dashboard', [
+            "title" => "Dashboard"
+        ]);
+    }
+}
