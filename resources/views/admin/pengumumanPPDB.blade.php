1f47f4cd805fe494d92a7ed94edc1455a3961c05
Zaini Ridha <zaiiridha8@gmail.com>
2024-04-24 00:06:21 +0700

    PPDB & Program Keahlian

--- resources/views/admin/pengumumanPPDB.blade.php
+++ resources/views/admin/pengumumanPPDB.blade.php
@@ -2,4 +2,297 @@
 
 @section('main-content')
 
+<div class="grid grid-cols-9 shadow-xl rounded-md">
+
+    <div class="col-span-2 col-start-2">
+        <button class="btn w-full hover:animate-pulse" onclick="my_modal_add.showModal()">
+            <i class="fas fa-upload"></i>
+            Upload Data
+        </button>
+    </div>
+
+    <!-- Search Bar -->
+    <div class="col-span-2 col-start-7">
+        <label class="input input-bordered flex items-center gap-2  focus-within:outline-none">
+            <i class="fas fa-magnifying-glass"></i>
+            <input type="text" class="grow" placeholder="Cari" />
+        </label>
+    </div>
+    <!-- Search Bar -->
+
+    <!-- Content -->
+    <div class="col-span-9 row-start-2">
+        <div class="overflow-x-auto mt-5 px-16">
+            <table class="table text-center">
+                <!-- head -->
+                <thead>
+                    <tr>
+                        <th>
+                            <label>
+                                <input type="checkbox" class="checkbox" />
+                            </label>
+                        </th>
+                        <th>No.</th>
+                        <th>Nama</th>
+                        <th>Jenis Kelamin</th>
+                        <Th>Nisn</Th>
+                        <Th>Agama</Th>
+                        <Th>No Telepon</Th>
+                        <Th>Aksi</Th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- row 1 -->
+                    <tr class="hover">
+                        <th class="w-8">
+                            <label>
+                                <input type="checkbox" class="checkbox" />
+                            </label>
+                        </th>
+                        <th class="w-8">1</th>
+                        <td class="w-8">James</td>
+                        <td class="w-8">Laki-Laki</td>
+                        <td class="w-8">1400420009920</td>
+                        <td class="w-8">Sedang Mencari</td>
+                        <td class="w-8">+1 0099 666 888</td>
+                        <td class="w-8">
+                            <details class="dropdown dropdown-right">
+                                <summary tabindex="0" role="button" class="btn btn-ghost button w-20">
+                                    <i class="fas fa-circle text-[0.5rem] circle-1 transition-all duration-500"></i>
+                                    <i class="fas fa-circle text-[0.5rem] circle-2 transition-all duration-500"></i>
+                                    <i class="fas fa-circle text-[0.5rem] circle-3 transition-all duration-500"></i>
+                                    <i class="fas fa-times font-bold text-xl hidden transition-all duration-500"></i>
+                                </summary>
+                                <ul tabindex="0" class="dropdown-content z-50 menu p-2 shadow bg-base-100 rounded-box w-32">
+                                    <!-- Edit -->
+                                    <li>
+                                        <button class="btn btn-ghost w-full hover:animate-pulse" onclick="my_modal_edit.showModal()">
+                                            <i class="fas fa-pen-to-square"></i>
+                                            Edit
+                                        </button>
+                                    </li>
+                                    <!-- Edit -->
+
+                                    <!-- View -->
+                                    <li>
+                                        <button class="btn btn-ghost w-full hover:animate-pulse" onclick="my_modal_view.showModal()">
+                                            <i class="fas fa-circle-info"></i>
+                                            Detail
+                                        </button>
+                                    </li>
+                                    <!-- View -->
+
+                                    <!-- Delete -->
+                                    <li>
+                                        <button class="btn btn-ghost w-full hover:animate-pulse" onclick="my_modal_delete.showModal()">
+                                            <i class="fas fa-trash"></i>
+                                            Hapus
+                                        </button>
+                                    </li>
+                                    <!-- Delete -->
+                                </ul>
+                            </details>
+                        </td>
+                    </tr>
+                    <!-- row 2 -->
+                    <tr class="hover">
+                        <th class="w-8">
+                            <label>
+                                <input type="checkbox" class="checkbox" />
+                            </label>
+                        </th>
+                        <th class="w-8">2</th>
+                        <td class="w-8">Rodygro</td>
+                        <td class="w-8">Laki-Laki</td>
+                        <td class="w-8">1400420009920</td>
+                        <td class="w-8">Sedang Mencari</td>
+                        <td class="w-8">+1 0099 666 888</td>
+                        <td class="w-8">
+                            <details class="dropdown dropdown-right">
+                                <summary tabindex="0" role="button" class="btn btn-ghost button w-20">
+                                    <i class="fas fa-circle text-[0.5rem] circle-1 transition-all duration-500"></i>
+                                    <i class="fas fa-circle text-[0.5rem] circle-2 transition-all duration-500"></i>
+                                    <i class="fas fa-circle text-[0.5rem] circle-3 transition-all duration-500"></i>
+                                    <i class="fas fa-times font-bold text-xl hidden transition-all duration-500"></i>
+                                </summary>
+                                <ul tabindex="0" class="dropdown-content z-50 menu p-2 shadow bg-base-100 rounded-box w-32">
+                                    <!-- Edit -->
+                                    <li>
+                                        <button class="btn btn-ghost w-full hover:animate-pulse" onclick="my_modal_edit.showModal()">
+                                            <i class="fas fa-pen-to-square"></i>
+                                            Edit
+                                        </button>
+                                    </li>
+                                    <!-- Edit -->
+
+                                    <!-- View -->
+                                    <li>
+                                        <button class="btn btn-ghost w-full hover:animate-pulse" onclick="my_modal_view.showModal()">
+                                            <i class="fas fa-circle-info"></i>
+                                            Detail
+                                        </button>
+                                    </li>
+                                    <!-- View -->
+
+                                    <!-- Delete -->
+                                    <li>
+                                        <button class="btn btn-ghost w-full hover:animate-pulse" onclick="my_modal_delete.showModal()">
+                                            <i class="fas fa-trash"></i>
+                                            Hapus
+                                        </button>
+                                    </li>
+                                    <!-- Delete -->
+                                </ul>
+                            </details>
+                        </td>
+                    </tr>
+                    <tr class="hover">
+                        <th class="w-8">
+                            <label>
+                                <input type="checkbox" class="checkbox" />
+                            </label>
+                        </th>
+                        <th class="w-8">3</th>
+                        <td class="w-8">Vinicius Jr</td>
+                        <td class="w-8">Laki-Laki</td>
+                        <td class="w-8">1400420009920</td>
+                        <td class="w-8">Sedang Mencari</td>
+                        <td class="w-8">+1 0099 666 888</td>
+                        <td class="w-8">
+                            <details class="dropdown dropdown-right">
+                                <summary tabindex="0" role="button" class="btn btn-ghost button w-20">
+                                    <i class="fas fa-circle text-[0.5rem] circle-1 transition-all duration-500"></i>
+                                    <i class="fas fa-circle text-[0.5rem] circle-2 transition-all duration-500"></i>
+                                    <i class="fas fa-circle text-[0.5rem] circle-3 transition-all duration-500"></i>
+                                    <i class="fas fa-times font-bold text-xl hidden transition-all duration-500"></i>
+                                </summary>
+                                <ul tabindex="0" class="dropdown-content z-50 menu p-2 shadow bg-base-100 rounded-box w-32">
+                                    <!-- Edit -->
+                                    <li>
+                                        <button class="btn btn-ghost w-full hover:animate-pulse" onclick="my_modal_edit.showModal()">
+                                            <i class="fas fa-pen-to-square"></i>
+                                            Edit
+                                        </button>
+                                    </li>
+                                    <!-- Edit -->
+
+                                    <!-- View -->
+                                    <li>
+                                        <button class="btn btn-ghost w-full hover:animate-pulse" onclick="my_modal_view.showModal()">
+                                            <i class="fas fa-circle-info"></i>
+                                            Detail
+                                        </button>
+                                    </li>
+                                    <!-- View -->
+
+                                    <!-- Delete -->
+                                    <li>
+                                        <button class="btn btn-ghost w-full hover:animate-pulse" onclick="my_modal_delete.showModal()">
+                                            <i class="fas fa-trash"></i>
+                                            Hapus
+                                        </button>
+                                    </li>
+                                    <!-- Delete -->
+                                </ul>
+                            </details>
+                        </td>
+                    </tr>
+
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>
+                            <label>
+                                <input type="checkbox" class="checkbox" />
+                            </label>
+                        </th>
+                        <th>No.</th>
+                        <th>Nama</th>
+                        <th>Jenis Kelamin</th>
+                        <Th>Nisn</Th>
+                        <Th>Agama</Th>
+                        <Th>No Telepon</Th>
+                        <Th>Aksi</Th>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+</div>
+
+<dialog id="my_modal_add" class="modal">
+    <div class="modal-box">
+        <form method="dialog">
+            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <h3 class="font-bold text-lg">Upload Data</h3>
+
+        <div class="grid grid-cols-3 w-52 -mt-5">
+            <div class="divider"></div>
+            <div class="divider divider-success"></div>
+            <div class="divider"></div>
+        </div>
+
+        <form action="">
+
+            <label class="input bg-transparent border-2 border-elm flex items-center gap-2 mb-5 w-full focus-within:outline-none">
+                <input type="file" class="bg-transparent py-2" name="excel_file" accept=".xlsx, .xls" placeholder="Upload file Excel" />
+            </label>
+
+            <div class="flex justify-end items-end mt-20 gap-4">
+
+                <button type="reset" class="btn bg-error w-32 h-10 rounded-sm border-none text-white mt-auto hover:text-error">
+                    <i class="fas fa-times"></i>
+                    Reset
+                </button>
+
+                <a href="" class="">
+                    <button type="submit" class="btn bg-elm w-32 h-10 rounded-sm border-none text-white mt-auto hover:text-elm">
+                        <i class=" fas fa-plus"></i>
+                        Tambah
+                    </button>
+                </a>
+
+            </div>
+
+        </form>
+    </div>
+</dialog>
+
+<dialog id="my_modal_edit" class="modal">
+    <div class="modal-box">
+        <form method="dialog">
+            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <h3 class="font-bold text-lg">Edit Data</h3>
+        <form action="">
+
+        </form>
+    </div>
+</dialog>
+
+<dialog id="my_modal_view" class="modal">
+    <div class="modal-box">
+        <form method="dialog">
+            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <h3 class="font-bold text-lg">Info Detail Data</h3>
+        <form action="">
+
+        </form>
+    </div>
+</dialog>
+
+<dialog id="my_modal_delete" class="modal">
+    <div class="modal-box">
+        <form method="dialog">
+            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <h3 class="font-bold text-lg">Hapus Data</h3>
+        <form action="">
+
+        </form>
+    </div>
+</dialog>
+
 @endsection
